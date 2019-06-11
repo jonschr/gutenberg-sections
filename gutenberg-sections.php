6d806394cc8633b1979143991dc0ad5931c351c8
jonschr <jonschr@gmail.com>
2019-06-11 14:07:56 -0500

    Bugfixes to alignment

--- gutenberg-sections.php
+++ gutenberg-sections.php
@@ -4,7 +4,7 @@
     Plugin URI: https://elod.in
     GitHub Plugin URI: https://github.com/jonschr/gutenberg-sections
     Description: Preset layouts for Gutenberg, using ACF for rendering
-    Version: 0.10.2
+    Version: 0.10.3
     Author: Jon Schroeder
     Author URI: https://elod.in
 
